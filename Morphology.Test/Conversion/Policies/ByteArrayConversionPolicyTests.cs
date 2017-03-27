2889ff264eca2ddc365fb6824b162096a466d885
Petr Drastil <petr.drastil@gmail.com>
2017-03-28 01:02:49 +0200

    Conversion - Added conversion policy for byte arrays

---
+++ Morphology.Test/Conversion/Policies/ByteArrayConversionPolicyTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using Moq;
+using Morphology.Conversion;
+using Morphology.Conversion.Policies;
+using Morphology.Conversion.Tokens;
+using Xunit;
+
+namespace Morphology.Test.Conversion.Policies
+{
+    public class ByteArrayConversionPolicyTests
+    {
+        [Fact]
+        public void TryConvert_ByteArray_ReturnsScalarToken()
+        {
+            var bytes = new byte[] {1, 2, 3};
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new ByteArrayConversionPolicy();
+
+            IPropertyToken token;
+            Assert.True(policy.TryConvert(converter, bytes, out token));
+
+            var scalar = token as ScalarToken;
+            Assert.NotNull(scalar);
+            Assert.Equal(bytes, scalar.Value);
+        }
+
+        [Fact]
+        public void TryConvert_IntArray_ReturnsFalse()
+        {
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new ByteArrayConversionPolicy();
+
+            IPropertyToken token;
+            Assert.False(policy.TryConvert(converter, new int[0], out token));
+        }
+
+        [Fact]
+        public void TryConvert_LargeByteArray_ReturnsScalarToken()
+        {
+            var bytes = Enumerable.Range(0, 1025).Select(b => (byte) b).ToArray();
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new ByteArrayConversionPolicy();
+
+            IPropertyToken token;
+            Assert.True(policy.TryConvert(converter, bytes, out token));
+
+            var scalar = token as ScalarToken;
+            Assert.NotNull(scalar);
+
+            string value = scalar.Value as string;
+            Assert.NotNull(value);
+            Assert.True(value.StartsWith("0x:"));
+            Assert.True(value.EndsWith("(1025 bytes)"));
+        }
+
+        [Fact]
+        public void TryConvert_NullConverter_ThrowsArgumentNullException()
+        {
+            var policy = new ByteArrayConversionPolicy();
+
+            IPropertyToken token;
+            Assert.Throws<ArgumentNullException>(() => policy.TryConvert(null, null, out token));
+        }
+
+        [Fact]
+        public void TryConvert_NullValue_ReturnsFalse()
+        {
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new ByteArrayConversionPolicy();
+
+            IPropertyToken token;
+            Assert.False(policy.TryConvert(converter, null, out token));
+        }
+    }
+}
---
+++ Morphology/Conversion/Policies/ByteArrayConversionPolicy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Morphology.Conversion.Tokens;
+
+namespace Morphology.Conversion.Policies
+{
+    /// <summary>
+    /// Determine if the supplied value is represented as Scalar token.
+    /// </summary>
+    public sealed class ByteArrayConversionPolicy : IConversionPolicy
+    {
+        #region Private Fields
+
+        //TODO this should be somehow dynamically configurable
+        private const int MaximumByteArrayLength = 1024;
+
+        #endregion
+
+        #region IConversionPolicy
+
+        /// <summary>
+        /// If supported, convert the provided value into a <see cref="IPropertyToken" />.
+        /// </summary>
+        /// <param name="converter">Converter for conversion of additional values.</param>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="result">Value converted to <see cref="IPropertyToken" /> if conversion was successful.</param>
+        /// <returns><c>true</c> if the value could be converted under this policy; <c>false</c> otherwise.</returns>
+        public bool TryConvert(IPropertyConverter converter, object value, out IPropertyToken result)
+        {
+            result = null;
+
+            if (converter == null) throw new ArgumentNullException(nameof(converter));
+
+            var bytes = value as byte[];
+            if (bytes == null) return false;
+
+            if (bytes.Length > MaximumByteArrayLength)
+            {
+                string hexValue = string.Concat(bytes.Take(16).Select(b => b.ToString("X2")));
+                string description = $"0x: {hexValue}... ({bytes.Length} bytes)";
+                result = new ScalarToken(description);
+            }
+            else
+            {
+                result = new ScalarToken(bytes.ToArray());
+            }
+
+            return true;
+        }
+
+        #endregion
+    }
+}
