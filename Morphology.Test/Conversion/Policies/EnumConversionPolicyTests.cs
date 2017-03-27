bc48d6aaaf2e1b92a523a93bb168cbf39f2c784c
Petr Drastil <petr.drastil@gmail.com>
2017-03-27 23:05:41 +0200

    Conversion - Added conversion policy for enums

---
+++ Morphology.Test/Conversion/Policies/EnumConversionPolicyTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Moq;
+using Morphology.Conversion;
+using Morphology.Conversion.Policies;
+using Morphology.Conversion.Tokens;
+using Xunit;
+
+namespace Morphology.Test.Conversion.Policies
+{
+    public class EnumConversionPolicyTests
+    {
+        private enum FooBar
+        {
+            Foo
+        }
+
+        [Fact]
+        public void TryConvert_Enum_ReturnsScalarToken()
+        {
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new EnumConversionPolicy();
+
+            IPropertyToken token;
+            Assert.True(policy.TryConvert(converter, FooBar.Foo, out token));
+
+            var scalar = token as ScalarToken;
+            Assert.NotNull(scalar);
+            Assert.Equal(FooBar.Foo, scalar.Value);
+        }
+
+        [Fact]
+        public void TryConvert_NullConverter_ThrowsArgumentNullException()
+        {
+            var policy = new EnumConversionPolicy();
+
+            IPropertyToken token;
+            Assert.Throws<ArgumentNullException>(() => policy.TryConvert(null, null, out token));
+        }
+
+        [Fact]
+        public void TryConvert_NullValue_ReturnsFalse()
+        {
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new EnumConversionPolicy();
+
+            IPropertyToken token;
+            Assert.False(policy.TryConvert(converter, null, out token));
+        }
+
+        [Fact]
+        public void TryConvert_ScalarValue_ReturnsFalse()
+        {
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new EnumConversionPolicy();
+
+            IPropertyToken token;
+            Assert.False(policy.TryConvert(converter, false, out token));
+        }
+    }
+}
---
+++ Morphology/Conversion/Policies/EnumConversionPolicy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using Morphology.Conversion.Tokens;
+
+namespace Morphology.Conversion.Policies
+{
+    /// <summary>
+    /// Determine if the supplied value is represented as Scalar value.
+    /// </summary>
+    public sealed class EnumConversionPolicy : IConversionPolicy
+    {
+        #region IConversionPolicy
+
+        /// <summary>
+        /// If supported, convert the provided value into a <see cref="IPropertyToken" />.
+        /// </summary>
+        /// <param name="converter">Converter for conversion of additional values.</param>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="result">Value converted to <see cref="IPropertyToken" /> if conversion was successful.</param>
+        /// <returns><c>true</c> if the value could be converted under this policy; <c>false</c> otherwise.</returns>
+        public bool TryConvert(IPropertyConverter converter, object value, out IPropertyToken result)
+        {
+            result = null;
+
+            if (converter == null) throw new ArgumentNullException(nameof(converter));
+
+            if (value == null) return false;
+            if (!value.GetType().GetTypeInfo().IsEnum) return false;
+
+            result = new ScalarToken(value);
+            return true;
+        }
+
+        #endregion
+    }
+}
