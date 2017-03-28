d97242af6932cd3e1882ed09f0380b8131c2ee74
Petr Drastil <petr.drastil@gmail.com>
2017-03-28 20:50:58 +0200

    Conversion - Added conversion policy for delegates

---
+++ Morphology.Test/Conversion/Policies/DelegateConversionPolicyTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Moq;
+using Morphology.Conversion;
+using Morphology.Conversion.Policies;
+using Morphology.Conversion.Tokens;
+using Xunit;
+
+namespace Morphology.Test.Conversion.Policies
+{
+    public class DelegateConversionPolicyTests
+    {
+        [Fact]
+        public void TryConvert_Delegate_ReturnsScalarToken()
+        {
+            Action<string> action = Console.WriteLine;
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new DelegateConversionPolicy();
+
+            IPropertyToken token;
+            Assert.True(policy.TryConvert(converter, action, out token));
+
+            var scalar = token as ScalarToken;
+            Assert.NotNull(scalar);
+
+            string value = scalar.Value as string;
+            Assert.NotNull(value);
+            Assert.True(value.Contains(nameof(Console.WriteLine)));
+        }
+
+
+        [Fact]
+        public void TryConvert_NullConverter_ThrowsArgumentNullException()
+        {
+            var policy = new DelegateConversionPolicy();
+
+            IPropertyToken token;
+            Assert.Throws<ArgumentNullException>(() => policy.TryConvert(null, null, out token));
+        }
+
+        [Fact]
+        public void TryConvert_NullValue_ReturnsFalse()
+        {
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new DelegateConversionPolicy();
+
+            IPropertyToken token;
+            Assert.False(policy.TryConvert(converter, null, out token));
+        }
+
+
+        [Fact]
+        public void TryConvert_Object_ReturnsFalse()
+        {
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new DelegateConversionPolicy();
+
+            IPropertyToken token;
+            Assert.False(policy.TryConvert(converter, new object(), out token));
+        }
+    }
+}
---
+++ Morphology/Conversion/Policies/DelegateConversionPolicy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using Morphology.Conversion.Tokens;
+
+namespace Morphology.Conversion.Policies
+{
+    /// <summary>
+    /// Determine if value can be represented as Scalar token
+    /// </summary>
+    public sealed class DelegateConversionPolicy : IConversionPolicy
+    {
+        /// <summary>
+        /// If supported, convert the provided value into a <see cref="IPropertyToken" />.
+        /// </summary>
+        /// <param name="converter">Converter for conversion of additional values.</param>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="result">Value converted to <see cref="IPropertyToken" /> if conversion was successful.</param>
+        /// <returns><c>true</c> if the value could be converted under this policy; <c>false</c> otherwise.</returns>
+        public bool TryConvert(IPropertyConverter converter, object value, out IPropertyToken result)
+        {
+            result = null;
+
+            if (converter == null) throw new ArgumentNullException(nameof(converter));
+
+            var del = value as Delegate;
+            if (del == null) return false;
+
+            result = new ScalarToken(del.GetMethodInfo().ToString());
+            return true;
+        }
+    }
+}
