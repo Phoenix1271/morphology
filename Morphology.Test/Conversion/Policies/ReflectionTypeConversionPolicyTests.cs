5f806362ec694ebb4fb6e32b27be3fd190650979
Petr Drastil <petr.drastil@gmail.com>
2017-03-28 21:04:48 +0200

    Conversion - Added conversion policy for reflection types

---
+++ Morphology.Test/Conversion/Policies/ReflectionTypeConversionPolicyTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Moq;
+using Morphology.Conversion;
+using Morphology.Conversion.Policies;
+using Morphology.Conversion.Tokens;
+using Xunit;
+
+namespace Morphology.Test.Conversion.Policies
+{
+    public class ReflectionTypeConversionPolicyTests
+    {
+        [Fact]
+        public void TryConvert_NullConverter_ThrowsArgumentNullException()
+        {
+            var policy = new ReflectionTypeConversionPolicy();
+
+            IPropertyToken token;
+            Assert.Throws<ArgumentNullException>(() => policy.TryConvert(null, null, out token));
+        }
+
+        [Fact]
+        public void TryConvert_NullValue_ReturnsFalse()
+        {
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new ReflectionTypeConversionPolicy();
+
+            IPropertyToken token;
+            Assert.False(policy.TryConvert(converter, null, out token));
+        }
+
+        [Fact]
+        public void TryConvert_Object_ReturnsFalse()
+        {
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new ReflectionTypeConversionPolicy();
+
+            IPropertyToken token;
+            Assert.False(policy.TryConvert(converter, new object(), out token));
+        }
+
+        [Fact]
+        public void TryConvert_ReflectionType_ReturnsScalarToken()
+        {
+            var converter = Mock.Of<IPropertyConverter>();
+            var policy = new ReflectionTypeConversionPolicy();
+
+            IPropertyToken token;
+            Assert.True(policy.TryConvert(converter, typeof(string), out token));
+
+            var scalar = token as ScalarToken;
+            Assert.NotNull(scalar);
+            Assert.Equal(typeof(string).ToString(), scalar.Value);
+        }
+    }
+}
---
+++ Morphology/Conversion/Policies/ReflectionTypeConversionPolicy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using Morphology.Conversion.Tokens;
+
+namespace Morphology.Conversion.Policies
+{
+    public sealed class ReflectionTypeConversionPolicy : IConversionPolicy
+    {
+        public bool TryConvert(IPropertyConverter converter, object value, out IPropertyToken result)
+        {
+            result = null;
+
+            if (converter == null) throw new ArgumentNullException(nameof(converter));
+
+            // These types and their subclasses are property-laden and deep
+            // Most of targets will convert them to strings
+            if (!(value is Type) && !(value is MemberInfo)) return false;
+
+            result = new ScalarToken(value.ToString());
+            return true;
+        }
+    }
+}
