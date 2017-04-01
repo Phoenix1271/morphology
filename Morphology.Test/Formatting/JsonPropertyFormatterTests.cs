511983652d03fa747e9f4926344e3397e40dc78b
Petr Drastil <petr.drastil@gmail.com>
2017-04-02 00:45:27 +0200

    Formatting - Added JsonPropertyFormatter

---
+++ Morphology.Test/Formatting/JsonPropertyFormatterTests.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Morphology.Conversion.Tokens;
+using Morphology.Formatting;
+using Morphology.Test.Support;
+using Xunit;
+
+namespace Morphology.Test.Formatting
+{
+    public class JsonPropertyFormatterTests
+    {
+        private class Nonformatable
+        {
+            #region Public Methods
+
+            public override string ToString()
+            {
+                return "Hello world";
+            }
+
+            #endregion
+        }
+
+        [Fact]
+        public void Format_DecimalScalarToken_IsFormatedAsNumber()
+        {
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output);
+            decimal value = Some.Decimal();
+            var token = new ScalarToken(value);
+
+            token.Render(formater);
+
+            Assert.Equal(value.ToString(CultureInfo.InvariantCulture), output.ToString());
+        }
+
+        [Fact]
+        public void Format_DecimalScalarToken_IsFormatedUsingSpecificCulture()
+        {
+            var formatProvider = new CultureInfo("cs-CZ");
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output, formatProvider);
+            decimal value = Some.Decimal();
+            var token = new ScalarToken(value);
+
+            token.Render(formater);
+
+            Assert.Equal(value.ToString(formatProvider), output.ToString());
+        }
+
+        [Fact]
+        public void Format_EmptySequenceToken_IsFormattedAsArray()
+        {
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output);
+            var token = new SequenceToken(null);
+
+            token.Render(formater);
+
+            Assert.Equal("[]", output.ToString());
+        }
+
+        [Fact]
+        public void Format_IntScalarToken_IsFormatedAsNumber()
+        {
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output);
+            decimal value = Some.Int();
+            var token = new ScalarToken(value);
+
+            token.Render(formater);
+
+            Assert.Equal(value.ToString(CultureInfo.InvariantCulture), output.ToString());
+        }
+
+        [Fact]
+        public void Format_NonEmptySequenceToken_IsFormattedAsArray()
+        {
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output);
+            var token = new SequenceToken(new[] {new ScalarToken(1), new ScalarToken("foo")});
+
+            token.Render(formater);
+
+            Assert.Equal("[1, \"foo\"]", output.ToString());
+        }
+
+        [Fact]
+        public void Format_NonFormattableScalarToken_IsFormatedFromToString()
+        {
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output);
+            var value = new Nonformatable();
+            var token = new ScalarToken(value);
+
+            token.Render(formater);
+
+            Assert.Equal(value.ToString(), output.ToString());
+        }
+
+        [Fact]
+        public void Format_NullScalarToken_IsFormatedAsNullString()
+        {
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output);
+            var token = new ScalarToken(null);
+
+            token.Render(formater);
+
+            Assert.Equal("null", output.ToString());
+        }
+
+        [Fact]
+        public void Format_StringScalarToken_SpecialCharactersAreEscaped()
+        {
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output);
+            var token = new ScalarToken(" \" ");
+
+            token.Render(formater);
+
+            Assert.Equal("\" \\\" \"", output.ToString());
+        }
+
+        [Fact]
+        public void JsonPropertyFormatter_OutputIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new JsonPropertyFormatter(null));
+        }
+
+        [Fact]
+        public void Render_EmptyDictionaryToken_IsFormattedAsEmptyArray()
+        {
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output);
+            var token = new DictionaryToken(null);
+
+            token.Render(formater);
+
+            Assert.Equal("[]", output.ToString());
+        }
+
+        [Fact]
+        public void Render_NonEmptyDictionaryToken_IsFormattedAsArrayOfKeyValuePairs()
+        {
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output);
+            var token = new DictionaryToken(new[]
+            {
+                new KeyValuePair<ScalarToken, IPropertyToken>(
+                    new ScalarToken("foo"), new ScalarToken(1)),
+                new KeyValuePair<ScalarToken, IPropertyToken>(
+                    new ScalarToken("bar"), new SequenceToken(new[] {new ScalarToken(1.2)}))
+            });
+
+            token.Render(formater);
+
+            Assert.Equal("[{\"key\": \"foo\", \"value\": 1}, {\"key\": \"bar\", \"value\": [1.2]}]", output.ToString());
+        }
+
+        [Fact]
+        public void Render_Property_IsFormattedAsProperty()
+        {
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output);
+            var property = new Property("property", new ScalarToken(1));
+
+            property.Render(formater);
+
+            Assert.Equal("\"property\": 1", output.ToString());
+        }
+
+        [Fact]
+        public void Render_StructureToken_IsFormattedAsSetOfProperties()
+        {
+            var output = new StringWriter();
+            var formater = new JsonPropertyFormatter(output);
+            var token = new StructureToken(new[]
+            {
+                new Property("Foo", new ScalarToken(1)),
+                new Property("Bar", new ScalarToken("bar"))
+            }, "MyType");
+
+
+            token.Render(formater);
+
+            Assert.Equal("\"MyType\": { \"Foo\": 1, \"Bar\": \"bar\" }", output.ToString());
+        }
+    }
+}
---
+++ Morphology/Formatting/JsonPropertyFormatter.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Morphology.Conversion.Tokens;
+
+namespace Morphology.Formatting
+{
+    public class JsonPropertyFormatter : IPropertyFormatter
+    {
+        #region Private Fields
+
+        private readonly IFormatProvider _formatProvider;
+        private readonly TextWriter _output;
+
+        #endregion
+
+        #region Constructors
+
+        public JsonPropertyFormatter(TextWriter output, CultureInfo formatProvider = null)
+        {
+            _output = output ?? throw new ArgumentNullException(nameof(output));
+            _formatProvider = formatProvider ?? CultureInfo.InvariantCulture;
+        }
+
+        #endregion
+
+        #region IPropertyFormater
+
+        public void Format(ScalarToken token)
+        {
+            if (token.Value == null)
+            {
+                _output.Write("null");
+                return;
+            }
+
+            string text = token.Value as string;
+            if (text != null)
+            {
+                _output.Write("\"");
+                _output.Write(text.Replace("\"", "\\\""));
+                _output.Write("\"");
+                return;
+            }
+
+            //If object has custom formatter then use that one
+            var custom = (ICustomFormatter) _formatProvider.GetFormat(typeof(ICustomFormatter));
+            if (custom != null)
+            {
+                _output.Write(custom.Format(null, token.Value, _formatProvider));
+                return;
+            }
+
+            // Use defaults for all other formatting.
+            var f = token.Value as IFormattable;
+            _output.Write(f?.ToString(null, _formatProvider) ?? token.Value.ToString());
+        }
+
+        public void Format(DictionaryToken token)
+        {
+            _output.Write('[');
+            var elements = token.Elements.ToArray();
+            int allButLast = elements.Length - 1;
+            for (int i = 0; i < allButLast; i++)
+            {
+                _output.Write("{\"key\": ");
+                elements[i].Key.Render(this);
+                _output.Write(", \"value\": ");
+                elements[i].Value.Render(this);
+                _output.Write("}, ");
+            }
+
+            if (elements.Length > 0)
+            {
+                var last = elements[elements.Length - 1];
+                _output.Write("{\"key\": ");
+                last.Key.Render(this);
+                _output.Write(", \"value\": ");
+                last.Value.Render(this);
+                _output.Write('}');
+            }
+
+            _output.Write(']');
+        }
+
+        public void Format(SequenceToken token)
+        {
+            _output.Write('[');
+
+            int allButLast = token.Elements.Count - 1;
+            for (int i = 0; i < allButLast; ++i)
+            {
+                token.Elements[i].Render(this);
+                _output.Write(", ");
+            }
+
+            if (token.Elements.Count > 0)
+                token.Elements[token.Elements.Count - 1].Render(this);
+
+            _output.Write(']');
+
+        }
+
+        public void Format(StructureToken token)
+        {
+            if (token.TypeName != null)
+            {
+                _output.Write($"\"{token.TypeName}\": ");
+            }
+
+            _output.Write("{ ");
+            int allButLast = token.Properties.Count - 1;
+            for (int i = 0; i < allButLast; i++)
+            {
+                Format(token.Properties[i]);
+                _output.Write(", ");
+            }
+
+            if (token.Properties.Count > 0)
+            {
+                var last = token.Properties[token.Properties.Count - 1];
+                Format(last);
+            }
+
+            _output.Write(" }");
+        }
+
+        public void Format(IProperty property)
+        {
+            _output.Write($"\"{property.Name}\": ");
+            property.Token.Render(this);
+        }
+
+        #endregion
+    }
+}
