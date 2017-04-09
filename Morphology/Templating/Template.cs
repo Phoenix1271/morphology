c931806f808613de185440ed983bc5627098a59e
Petr Drastil <petr.drastil@gmail.com>
2017-04-09 16:33:12 +0200

    Templating - Added template representation

---
+++ Morphology/Templating/Template.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Morphology.Templating
+{
+    /// <summary>
+    /// Represent text template for given message
+    /// </summary>
+    public class TextTemplate
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Copy constructor that initializes a new instance of the <see cref="TextTemplate"/> class.
+        /// </summary>
+        internal TextTemplate(TextTemplate template)
+        {
+            Template = template.Template;
+            Tokens = template.Tokens;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextTemplate"/> class.
+        /// </summary>
+        /// <param name="temmplate">The underlying text temmplate.</param>
+        /// <param name="tokens">The tokens that belong to given text template.</param>
+        internal TextTemplate(string temmplate, IEnumerable<ITemplateToken> tokens)
+        {
+            Template = temmplate;
+            Tokens = tokens.ToArray();
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Underlying text template.
+        /// </summary>
+        public string Template { get; }
+
+        /// <summary>
+        /// Template tokens associated with template.
+        /// </summary>
+        public IReadOnlyList<ITemplateToken> Tokens { get; }
+
+        #endregion
+    }
+}
